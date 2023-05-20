94d2636886b06af39e2d52f6abf728fbf84fd9be
Benedict Harcourt <ben.harcourt@harcourtprogramming.co.uk>
2023-05-20 16:09:30 +0000

    Split schema up to make IDE tricks easier

--- convert.py
+++ convert.py
@@ -1,17 +1,29 @@
 #!/usr/bin/env python3
-# vim: nospell
 
 import json
+import pathlib
+import markdown
 import yaml
-import sys
 
 
-name = sys.argv[1]
-target = name.replace(".yaml", ".json").replace(".yml", ".json")
+path = pathlib.Path(".")
 
-print(name, "->", target)
+with (path / "character.yaml").open("r", encoding="utf-8") as infile:
+    character: dict[str, ...] = yaml.load(infile, Loader=yaml.CLoader)
 
-with open(name, "r") as infile:
-    data = yaml.load(infile, Loader=yaml.CLoader)
-    with open(target, "w") as outfile:
-        json.dump(data, outfile)
+for key in character:
+    if not isinstance(character[key], list):
+        continue
+
+    if (path / key).is_dir():
+        for file in (path / key).iterdir():
+            with file.open("r", encoding="utf-8") as infile:
+                character[key].extend(yaml.load_all(infile, Loader=yaml.CLoader))
+
+backstory: dict[str, str] = character["backstory"]
+for key, value in backstory.items():
+    backstory[key] = markdown.markdown(value)
+
+
+with (path / "character.json").open("w", encoding="utf-8") as outfile:
+    json.dump(character, outfile)
